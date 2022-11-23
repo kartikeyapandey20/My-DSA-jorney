38e99110ec7e6be3879f19543020f79b1ad10d94
Pandey Kartikeya Prashant <kartikeyapandey20@gmail.com>
2022-11-23 19:11:36 +0530

    added code for graph

---
+++ C++ language/graph/createGraphWithVector.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<vector<int>> printAdj(int n , int m ,vector <vector <int>> &edges){
+    vector<int> ans[n];
+    
+}
+int main(){
+    return 0;
+}
