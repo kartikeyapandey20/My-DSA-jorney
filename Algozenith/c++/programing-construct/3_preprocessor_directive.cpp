73a79912811c53bf3cbb6806d91278d0f02b4f14
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-04-26 01:50:49 +0530

    preprocessor directive and modulo

---
+++ Algozenith/c++/programing-construct/3_preprocessor_directive.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define printLine cout<< "-----------------------------------------------------" << "\n";
+
+using namespace std;
+
+int main(){
+    int x = 5;
+    char y = 'z';
+    printLine  
+    cout << "x: " << x << endl;
+    printLine  
+    cout << "y: " << y << endl;
+    printLine  
+    return 0;
+}
---
+++ Algozenith/c++/programing-construct/4_modulo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = -3;
+    
+    
+    // Modulo operation
+    int result = a % b;
+    # output will be 1
+    cout << "The result of " << a << " % " << b << " is: " << result << endl;
+    
+    return 0;
+}
