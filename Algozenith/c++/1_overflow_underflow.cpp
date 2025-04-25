8d3dd2eb2df8542d97ccfb143156bb4303418499
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-04-26 00:14:32 +0530

    resuming my DSA journey with algozenith

---
+++ Algozenith/c++/1_overflow_underflow.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int maxInt = INT_MAX;
+    int minInt = INT_MIN;
+
+    cout << "Max Int: " << maxInt << endl;
+    cout << "Min Int: " << minInt << endl;
+
+    cout << " Min int + Max int: " << minInt + maxInt << endl;
+    return 0;
+}
---
+++ Algozenith/c++/2_type_conversion.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x = 5;
+    char y = 'z';
+
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+    cout << (int)y << endl; 
+    cout << "x + y: " << x + y << endl;
+}
