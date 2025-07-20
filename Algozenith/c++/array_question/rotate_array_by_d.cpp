47a892cf0557328dd9ccd6575a9a4a9808ba65fe
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-07-20 22:59:17 +0530

    optimal approach

--- Algozenith/c++/array_question/rotate_array_by_d.cpp
+++ Algozenith/c++/array_question/rotate_array_by_d.cpp
@@ -48,8 +48,34 @@ vector<int> rotateBrute(vector<int> &nums , int k){
     return nums;
 }
 
+// Optimal Apporach
+vector<int> leftRotate(vector<int> &nums , int n , int k)
+{
+    cout << "\nArray Before Rotation\n";
+    printArray(nums);
+    vector<int> temp(3);
+
+    int d = k%nums.size();
+    
+    if(d==0){
+        cout << "\nArray After "<< k <<"Rotation\n";
+        printArray(nums);
+        return nums;
+    }
+
+    reverse(nums.begin() , nums.begin() + d);
+    reverse(nums.begin() + d, nums.begin() + n);
+    reverse(nums.begin() , nums.begin() + n);
+    cout << "\nArray After "<< k <<"Rotation\n";
+    printArray(nums);
+    return nums;
+}
 int main(){
     vector<int> arr = {1,2,3,4,5,6,7};
+    int n = arr.size();
+    cout <<"Brute force approach\n";
     rotateBrute(arr , 5);
-
+    vector<int> arr2 = {1,2,3,4,5,6,7};
+    cout <<"\nOptimal approach\n";
+    leftRotate(arr2, n , 5);
 }
