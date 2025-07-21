157701cb821c65c6d080ee504cc1fcda451f5b49
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-07-22 01:01:56 +0530

    optmial solution of union of array

--- Algozenith/c++/array_question/union_of_2_sorted_arrays.cpp
+++ Algozenith/c++/array_question/union_of_2_sorted_arrays.cpp
@@ -10,7 +10,8 @@ void printArray(vector<int> nums)
     }
 }
 
-void union_of_sorted_array_brute(vector<int> arr1 , vector<int> arr2)
+// Brute Force Approach
+void unionOfSortedArrayBrute(vector<int> arr1 , vector<int> arr2)
 {
     set<int> st;
 
@@ -31,11 +32,63 @@ void union_of_sorted_array_brute(vector<int> arr1 , vector<int> arr2)
     }    
     printArray(temp);
 }
+
+// Optimal Solution
+void sortedArray(vector<int> a, vector<int> b)
+{
+    int n1 = a.size();
+    int n2 = b.size();
+
+    int i = 0;
+    int j = 0;
+    vector<int> unionArr;
+
+    while(i < n1 && j < n2)
+    {
+        if(a[i] <= b[j])
+        {
+            if(unionArr.size() == 0 || unionArr.back() != a[i])
+            {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else
+        {
+            if(unionArr.size() == 0 || unionArr.back() != b[j])
+            {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+    while(j < n2)
+    {
+            if(unionArr.size() == 0 || unionArr.back() != b[j])
+            {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+    }
+    while(i < n1)
+    {
+        if(unionArr.size() == 0 || unionArr.back() != a[i])
+            {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+    }
+
+    printArray(unionArr);
+}
+
 int main(){
     vector<int> arr1 = {1,1,2,3,4,5};
     vector<int> arr2 = {2,3,4,4,5,6};
 
-    union_of_sorted_array_brute(arr1,arr2);
-
+    // Brute Force Solution
+    unionOfSortedArrayBrute(arr1,arr2);
+    // Optimal Solution
+    sortedArray(arr1,arr2);
     return 0;
 }
