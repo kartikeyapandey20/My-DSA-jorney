2d585784d2ce41518bfa65d575385d2a10c0c41f
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-07-23 00:48:15 +0530

    added better solutiion for missing number

--- Algozenith/c++/array_question/missing_number.cpp
+++ Algozenith/c++/array_question/missing_number.cpp
@@ -8,7 +8,7 @@ void missingNumberBrute(vector<int> arr , int n)
     for(int i = 1 ; i < n ; i++)
     {
         int flag = 0;
-        for(int j = 0; j < n - 1; j++)
+        for(int j = 0; j < n; j++)
         {
             if(arr[j] == i)
             {
@@ -22,11 +22,30 @@ void missingNumberBrute(vector<int> arr , int n)
             break;
         }
     }
-    cout  << "The Missing Number is " << ans;
+    cout  << "\nThe Missing Number is " << ans;
 }
 
-int main(){
-    vector<int> arr = {1,2,4,5};
-    missingNumberBrute(arr,arr.size());
+void missingNumberBetter(vector<int> arr , int n)
+{
+    int ans;
+    vector<int> hash(n+1,0);
+    for(int i = 0; i < n; i++)
+    {
+        hash[arr[i]] = 1;
+    }
 
+    for(int i = 0; i < n; i++)
+    {
+        if(hash[i] == 0)
+        {
+            ans =  i;
+            break;
+        }
+    }
+    cout  << "\nThe Missing Number is " << ans;
+}
+int main(){
+    vector<int> arr = {3,0,1};
+    missingNumberBrute(arr,arr.size());
+    missingNumberBetter(arr,arr.size());
 }
