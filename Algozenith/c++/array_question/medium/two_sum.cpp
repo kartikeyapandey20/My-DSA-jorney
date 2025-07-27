be453841bd3caa95e4d70ac3f47b6d0603e1cc21
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-07-27 15:24:20 +0530

    added better solution for two sum

--- Algozenith/c++/array_question/medium/two_sum.cpp
+++ Algozenith/c++/array_question/medium/two_sum.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 // Brute Force
 void twoSumBrute(vector<int> nums , int target){
-    int firstIndex , secondIndex;
+    int firstIndex = 0, secondIndex = 0;
     for(int i = 0; i < nums.size(); i++)
     {
-        for(int j =  i ; j < nums.size(); j++)
+        for(int j =  i + 1; j < nums.size(); j++)
         {
             if(nums[i] + nums[j] == target)
             {
@@ -22,8 +22,32 @@ void twoSumBrute(vector<int> nums , int target){
     cout << "Second value " << nums[secondIndex] << " At Index " << secondIndex <<endl;
 }
 
+// Better Solution
+void twoSumBetter(vector<int> nums , int target){
+    map<int,int> mpp;
+    int n = nums.size();
+    vector<int> ans = {0,0};
+    for(int i = 0; i < n; i++)
+    {
+        int num = nums[i];
+        int moreNeeded = target - num;
+        if(mpp.find(moreNeeded) !=mpp.end())
+        {
+            ans = {mpp[moreNeeded] , i};
+        } 
+        mpp[num] = i;
+    }
+
+    cout << "First value " << nums[ans[0]] << " At Index " << ans[0] <<endl;
+    cout << "Second value " << nums[ans[1]] << " At Index " << ans[1] <<endl;
+
+}
+
+
 int main(){
     vector<int> nums = {2,4,6,2,7};
+    // Brute Force Solution
     twoSumBrute(nums,8);
+    twoSumBetter(nums,8);
     return 0;
 }
