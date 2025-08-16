ebfdc7063d950100d4d59e89485bcfcfed758f54
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-08-16 18:51:08 +0530

    added better solution for majority elements

--- Algozenith/c++/array_question/medium/majority_elements.cpp
+++ Algozenith/c++/array_question/medium/majority_elements.cpp
@@ -26,11 +26,33 @@ int majorityElementBrute(vector<int> nums, int n)
     return -1;
 }
 
+
+int majorityElementBetter(vector<int> nums , int n)
+{
+    map<int , int > mpp;
+
+    for(int i = 0; i < n ; i ++)
+    {
+        mpp[nums[i]]++;
+    }
+
+    for(auto it : mpp)
+    {
+        if(it.second > n/2)
+        {
+            return it.first;
+        }
+    }
+    return -1;
+}
 int main(){
     vector<int> nums =  {2,2,1,1,1,2,2};
     int n = nums.size();
 
     int ans = majorityElementBrute(nums,n);
-    cout  << "Answer : " << ans;
+    cout  << "Answer : " << ans << endl; 
+
+    ans = majorityElementBetter(nums,n);
+    cout  << "Answer : " << ans <<  endl;
     return 0;
 }
