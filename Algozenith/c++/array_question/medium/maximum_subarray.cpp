b938eeb4ac4f7036c312b20a994ffff36d28463a
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-08-26 00:22:40 +0530

    added better solution for maximum subarray

--- Algozenith/c++/array_question/medium/maximum_subarray.cpp
+++ Algozenith/c++/array_question/medium/maximum_subarray.cpp
@@ -13,7 +13,7 @@ int maxSubArrayBrute(vector<int> &nums)
         {
             int sum = 0;
 
-            for (int k = i; k < j; k++)
+            for (int k = i; k <= j; k++)
             {
                 sum = sum + nums[k];
             }
@@ -26,8 +26,27 @@ int maxSubArrayBrute(vector<int> &nums)
 
 }
 
+int maxSubArrayBetter(vector<int> &nums)
+{
+    int n = nums.size();
+    int maximum = INT_MIN;
+
+    for(int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for(int j = i; j < n; j++)
+        {
+            sum = sum + nums[j];
+            maximum = max(sum , maximum);
+        }
+    }
+
+    return maximum;
+
+}
+
 int main(){
-    vector<int> nums = {-2,-3,4,-1,-2,1,5,-3};
+    vector<int> nums = {1};
 
     int ans = maxSubArrayBrute(nums);
 
