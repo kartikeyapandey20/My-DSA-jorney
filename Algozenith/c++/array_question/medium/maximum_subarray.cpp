605def5fb06cc45486c841d507eb3d7cc5324769
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-08-26 00:51:35 +0530

    added optimal solution for the maximum subarray

--- Algozenith/c++/array_question/medium/maximum_subarray.cpp
+++ Algozenith/c++/array_question/medium/maximum_subarray.cpp
@@ -45,6 +45,31 @@ int maxSubArrayBetter(vector<int> &nums)
 
 }
 
+int maxSubArray(vector<int> &nums)
+{
+    int n = nums.size();
+    int maximum = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++){
+        sum += nums[i];
+
+        if(sum > maximum){
+            maximum = sum;
+        }
+        if(sum < 0){
+            sum = 0;
+        }
+    }
+
+    if(maximum < 0){
+        maximum = 0;
+    }
+    return maximum;
+
+}
+
+// Not considering negative number as the maximum subarray
 int main(){
     vector<int> nums = {1};
 
@@ -55,5 +80,9 @@ int main(){
     ans = maxSubArrayBetter(nums);
 
     cout << "(Better)Maximum Sub Array as the sum of " << ans << endl;
+
+    ans = maxSubArray(nums);
+
+    cout << "Maximum Sub Array as the sum of " << ans << endl;
     return 0;
 }
