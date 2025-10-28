b2ba1d75705fdf55c6716f496578f59c77c51276
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-10-28 12:28:23 +0530

    added optimal solution for the next permutation

--- Algozenith/c++/array_question/medium/next_permutation.cpp
+++ Algozenith/c++/array_question/medium/next_permutation.cpp
@@ -18,10 +18,45 @@ void stlNextPermutation(vector<int> &nums)
 {
     next_permutation(nums.begin(),nums.end());
 }
+
+void nextPermutation(vector<int> &nums)
+{
+    int ind = -1;
+    int n = nums.size();
+    for(int i = n - 2; i >= 0;i--){
+        if(nums[i] < nums[i+1])
+        {
+            ind = i;
+            break;
+        }
+    }
+    
+    for(int i = n - 1; i > ind; i--){
+        if(nums[i] > nums[ind]){
+            swap(nums[i], nums[ind]);
+            break;
+        }
+    }
+
+    reverse(nums.begin() + ind + 1, nums.end());
+
+    if(ind == -1){
+        reverse(nums.begin(), nums.end());
+    }
+}
 int main(){
     vector<int> nums =  {1,2,3};
     
     stlNextPermutation(nums);
 
     printArray(nums);
+
+    nums =  {1,2,3};
+
+    nextPermutation(nums);
+
+    printArray(nums);
+
+    return 0;
+
 }
