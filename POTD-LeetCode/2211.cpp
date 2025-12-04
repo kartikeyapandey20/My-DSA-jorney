34870b1479867746d8d2c5295d94c6165a63ad26
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-12-05 00:38:42 +0530

    potd

---
+++ POTD-LeetCode/2211.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.length();
+        
+        int left = 0;
+        while (left < n && directions[left] == 'L') {
+            left++;
+        }
+        
+        int right = n - 1;
+        while (right >= 0 && directions[right] == 'R') {
+            right--;
+        }
+        
+        int collisions = 0;
+        for (int i = left; i <= right; i++) {
+            if (directions[i] != 'S') {
+                collisions++;
+            }
+        }
+        
+        return collisions;
+    }
+};
