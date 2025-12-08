f4c06b53b3ec1865063ba45ebfb423a81f5deb43
Kartikeya Pandey <kartikeyapandey20@gmail.com>
2025-12-08 19:55:43 +0530

    potd

---
+++ POTD-LeetCode/1925.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int count = 0;
+        
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int cSquared = a * a + b * b;
+                
+                int c = sqrt(cSquared);
+                
+                if (c * c == cSquared && c <= n) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
